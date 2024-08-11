c48987209c712daa4fb9aff92d4ab6910d0a6f69
Michael Byrd <iamtron01@hotmail.com>
2024-08-11 11:12:06 -0500

    housekeeping

--- 01 - Variables and data types.py
+++ 01 - Variables and data types.py
@@ -7,17 +7,29 @@ print(name_length)
 name_length = int("4")
 print(name_length)
 
-name_list = ["Parthos", "Athos", "Aramis", "D'Artagnan"]
+name_list = [
+    "Parthos",
+    "Athos",
+    "Aramis",
+    "D'Artagnan"]
 name1, name2, name3, name4 = name_list
 print(name1)
 print(name2)
 print(name3)
 print(name4)
 
-name_tuple = ("Parthos", "Athos", "Aramis", "D'Artagnan")
+name_tuple = (
+    "Parthos",
+    "Athos",
+    "Aramis",
+    "D'Artagnan")
 print(type(name_tuple))
 
-name_dictionary = {"Parthos": 1, "Athos": 2, "Aramis": 3, "D'Artagnan": 4}
+name_dictionary = {
+    "Parthos": 1,
+    "Athos": 2,
+    "Aramis": 3,
+    "D'Artagnan": 4}
 print(type(name_dictionary))
 
 name_boolean = True
